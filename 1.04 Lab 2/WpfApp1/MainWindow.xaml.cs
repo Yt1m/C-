71f14367e469c2ac400dc85d2db4fb17b949db21
Yt1m <144239923+Yt1m@users.noreply.github.com>
2024-10-30 16:18:03 +0200

    Add files via upload

---
+++ 1.04 Lab 2/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.Threading.Tasks;
+
+
+namespace PowerCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(NumberInput.Text, out double number) &&
+                int.TryParse(PowerInput.Text, out int power))
+            {
+                ResultTextBlock.Text = "Вычисляется...";
+
+                double result = await CalculatePowerAsync(number, power);
+
+                ResultTextBlock.Text = $"Результат: {result}";
+            }
+            else
+            {
+                ResultTextBlock.Text = "Введите корректные значения.";
+            }
+        }
+
+        private Task<double> CalculatePowerAsync(double number, int power)
+        {
+            return Task.Run(() => Math.Pow(number, power));
+        }
+    }
+}
